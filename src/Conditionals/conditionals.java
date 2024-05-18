632dab196be46a3a59fe98f741d145fe4d8c6e94
Dimo Yordanov <dimoy73@gmail.com>
2024-05-18 03:29:56 +0300

    Task 3 switch

--- src/Conditionals/conditionals.java
+++ src/Conditionals/conditionals.java
@@ -6,18 +6,54 @@ public class conditionals {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Task 2
 
-        int inputOne = Integer.parseInt(scan.next());
-        int inputTwo = Integer.parseInt(scan.next());
-
-        if (inputOne > inputTwo) {
-            System.out.println(inputOne);
-        } else if (inputTwo > inputOne) {
-            System.out.println(inputTwo);
-        } else {
-            System.out.println("Equal numbers");
+        //Task 3
+        String word = scan.nextLine();
+        switch (word) {
+            case "zero":
+                System.out.println(0);
+                break;
+            case "one":
+                System.out.println(1);
+                break;
+            case "two":
+                System.out.println(2);
+                break;
+            case "three":
+                System.out.println(3);
+                break;
+            case "four":
+                System.out.println(4);
+                break;
+            case "five":
+                System.out.println(5);
+                break;
+            case "six":
+                System.out.println(6);
+                break;
+            case "seven":
+                System.out.println(7);
+                break;
+            case "eight":
+                System.out.println(8);
+                break;
+            case "nine":
+                System.out.println(9);
+                break;
         }
+
+        // Task 2
+//
+//        int inputOne = Integer.parseInt(scan.next());
+//        int inputTwo = Integer.parseInt(scan.next());
+//
+//        if (inputOne > inputTwo) {
+//            System.out.println(inputOne);
+//        } else if (inputTwo > inputOne) {
+//            System.out.println(inputTwo);
+//        } else {
+//            System.out.println("Equal numbers");
+//        }
 
 
 

58c475d8680f290288300ab04dc57bdd41a9a5ac
Dimo Yordanov <dimoy73@gmail.com>
2024-05-23 01:22:49 +0300

    First loop exercise

---
+++ src/Loops/LoopsEasy.java
@@ -0,0 +1,26 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class LoopsEasy {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String numToCheck = scan.next();
+        int[] numbers = new int[numToCheck.length()];
+        for (int i = 0; i < numToCheck.length(); i++) {
+            numbers[i] = Integer.parseInt(String.valueOf(numToCheck.charAt(i)));
+        }
+
+        double sum = 0;
+        for (int num : numbers) {
+            sum += Math.pow(num, numToCheck.length());
+        }
+
+        if (sum == Integer.parseInt(numToCheck)) {
+            System.out.println("Armstrong number");
+        } else {
+            System.out.println("Not an Armstrong number");
+        }
+    }
+}

9bcbb4cadc45d69512861bcb941419c3c7551095
Dimo Yordanov <dimoy73@gmail.com>
2024-05-24 18:00:04 +0300

    First two array tasks

---
+++ src/ArraysAndLists/Arrays.java
@@ -0,0 +1,30 @@
+package ArraysAndLists;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //Task 2
+//        int indexNum = Integer.parseInt(scan.nextLine());
+//
+//        String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+//
+//        if (indexNum <= 7) {
+//            for (int i = 0; i < weekDays.length; i++) {
+//                if (i == indexNum) {
+//                    System.out.println(weekDays[i - 1]);
+//                }
+//            }
+//        } else {
+//            System.out.println("Invalid day");
+//        }
+
+        // Task 1
+//        int[] nums = {20,30,40};
+//
+//        int sum = nums[0] + nums[nums.length - 1];
+//        System.out.println(sum);
+    }
+}
